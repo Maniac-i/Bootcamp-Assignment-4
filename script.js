7e2facc676900f896cf26b3aa6bbc648c593b558
Joe Maniaci <joe.maniaci1@gmail.com>
2020-11-05 21:18:13 -0500

    implimented semantic html and added comments

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 
-
-//make inteface with html/css
-
-//start quiz button that stats quiz and timer
-
-//mkae the questions
-
-//questions change when answer is submitted
-
-//timeout for ending the quiz
-
-//place to enter score is prompted after quiz end
-
-//high scores are shown
-
+//global variables
 var theQuestion = document.getElementById("theQuestion");
 var firstAnswer = document.querySelector(".a");
 var secondAnswer = document.querySelector(".b");
@@ -37,8 +23,7 @@ var i = 0
 var toggle;
 
 
-//var highScores = localStorage.setItem("score", score);
-//localStorage.getItem(score);
+//An array containing the questions
 var questionArray = [
 
   questionOne = {
@@ -115,10 +100,12 @@ var questionArray = [
   },
 ]
 
+//sets first question to be displayed
 setFirstQuestion(0);
 
+//Function that sets the question and answers being displayed
 function setFirstQuestion(i) {
-  //sets inputs for first question
+ 
   theQuestion.textContent = questionArray[i].question;
   firstAnswer.textContent = questionArray[i].a;
   secondAnswer.textContent = questionArray[i].b;
@@ -126,27 +113,34 @@ function setFirstQuestion(i) {
   fourthAnswer.textContent = questionArray[i].d;
 }
 
-//hides main page and displays first question when Start Quiz is pressed
-//starts timer
+//set timeInterval as a global variable
 var timeInterval;
 
 startButton.addEventListener("click", function () {
 
+  //hides quiz start section and displays question section
   quiz.classList.add("hide");
   question.classList.remove("hide");
 
   timeDisplay.textContent = "Time: " + timeLeft;
 
+  //sets time interval
   timeInterval = setInterval(function () {
 
     timeLeft--;
     timeDisplay.textContent = "Time: " + timeLeft;
 
+    //conditional for when timer reaches 0
     if (timeLeft === 0) {
+
+      //set timer to 0
       timeDisplay.textContent = "Time: 0"
       clearInterval(timeInterval);
 
+      //conditional for when all the questions are answered before time runs out
       if (toggle !== "over") {
+
+        //shows finished section and hides question section
         finished.classList.remove("hide");
         question.classList.add("hide");
         clearInterval(timeInterval);
@@ -162,17 +156,26 @@ startButton.addEventListener("click", function () {
 //runs nextQuestion to dynamically display nexts questions text
 listGroup.addEventListener("click", function (event) {
 
+  //sets the text of answered selected to a variable
   var buttonText = event.target.textContent
 
+  //checks to see if answer selected matches the correct answer
   if (buttonText === questionArray[i].answer) {
+
+    //if answer is correct
     rightWrong.textContent = "Correct!";
     score++
+
   } else {
+
+    //if answer is wrong
     rightWrong.textContent = "wrong!";
     timeLeft = timeLeft - 10;
 
 
   }
+
+  //display next question
   nextQuestion();
 
 });
@@ -182,45 +185,53 @@ var nextQuestion = function () {
 
   i++;
 
-  //hides question section after last question --- NEED TO CHANGE TO FOR LOOP
   if (i < questionArray.length) {
-    setFirstQuestion(i);
 
+    setFirstQuestion(i);
+    
   } else {
+
+    //hides question section and displays finished sectionafter last question is answered
     clearInterval(timeInterval);
     question.classList.add("hide");
     finished.classList.remove("hide");
 
-
+    //Displays final score
     scoreEl.textContent = "Your Score is: " + score + " out of 5";
   }
 }
 
 
-//stores users initials and score 
+//stores users initials and score in local storage
 submit.addEventListener("click", function (event) {
 
   event.preventDefault();
 
+  //creates variable out of user input
   var initials = document.getElementById("inputPassword2").value;
 
+  //sets local storage items
   localStorage.setItem("score", score);
   localStorage.setItem("initials", initials);
 
+  //calls create list function
   createListItem();
 
 
 })
 
-//creates list item with score and appends to highscore list
+//creates list item with score and intials and appends to highscore list
 function createListItem() {
 
+  //hides finished section and shows highscore section
   highScoreSection.classList.remove("hide");
   finished.classList.add("hide");
 
+  //grabs initials and score values from local storage
   var user = localStorage.getItem("initials");
   var userScore = localStorage.getItem("score");
 
+  //creat a list item and appends it to highscore page
   listItem = document.createElement("li");
   listItem.textContent = user + ": " + userScore + " out of 5";
   highScores.appendChild(listItem);
@@ -228,18 +239,28 @@ function createListItem() {
 
 }
 
+//adds click event to Go Back button
 goBack.addEventListener("click", () => {
 
+  //resets counting vairables
   i = 0;
   timeLeft = 60;
   score = 0;
 
+  //hides highscore section and shows the Quiz start page
   highScoreSection.classList.add("hide");
   quiz.classList.remove("hide");
-  
+
+  //clears current time interval
   clearInterval(timeInterval);
+
+  //displays first question
   setFirstQuestion(0);
+
+  //clears time display
   timeDisplay.textContent = "";
+
+  //clears display showing if question was answered correctly or not
   rightWrong.textContent = "";
 
 })
